b0b269b2fd6c53c8d420c7fc897649fe1fe04ef6
Tubalcain <tumbalcain@gmail.com>
2025-11-04 16:08:01 +0700

    Setup AprilTag and VisionPortal

--- pipelines/AprilTagWebcam.java
+++ pipelines/AprilTagWebcam.java
@@ -1,5 +1,39 @@
 package main.java.org.firstinspires.ftc.teamcode.R2045.pipelines;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
 public class AprilTagWebcam {
-    
+    private AprilTagProcessor aprilTagProcessor;
+    private VisionPortal visionPortal;
+
+    private List<AprilTagDetection> detectedTags = new ArrayList<>();
+
+    private Telemetry telemetry;
+
+    public void init(HardwareMap hardwareMap, Telemetry, telemetry) {
+        this.telemetry = telemetry;
+
+        aprilTagProcessor = new AprilTagProcessor.Builder()
+            .setDrawTagID(true)
+            .setDrawTagOutline(true)
+            .setDrawAxes(true)
+            .setDrawCubeProjection(true)
+            .setOutputUnits(DistanceUnit.CM, AngleUnit.DEGREES)
+            .build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        builder.setCameraResolution(new Size(640, 480));
+        builder.addProcessor(aprilTagProcessor);
+
+        visionPortal = builder.build();
+    }
 }
