f70db46b673647dab0cff9caac556b4a793b3d9c
Tubalcain <tumbalcain@gmail.com>
2025-11-05 13:52:18 +0700

    Add comments at the end of block and Telemetry display function

--- pipelines/AprilTagWebcam.java
+++ pipelines/AprilTagWebcam.java
@@ -46,19 +46,19 @@ public class AprilTagWebcam {
         builder.addProcessor(aprilTagProcessor);
 
         visionPortal = builder.build();
-    }
+    } // end of init() function
 
     // Update Function
 
     public void update() {
         detectedTags = aprilTagProcessor.getDetections();
-    }
+    } // end of update() function
 
     // List of Detected AprilTags
 
     public List<AprilTagDetection> getDetectedTags() {
         return detectedTags;
-    }
+    } // end of getDetectedTag() List
 
     // Specific AprilTag Id Fetch Function
 
@@ -66,17 +66,35 @@ public class AprilTagWebcam {
         for (AprilTagDetection detection : detectedTags) {
             if (detection.id == id) {
                 return detection;
-            }
-        }
+            } // end of conditional
+        } // end of for loop
 
         return null;
-    }
+    } // end of getTagBySpecificId() function
+
+    // Telemetry Display Function
+
+    public void displayDetectionTelemetry(AprilTagDetection detectedId) {
+        if (detectedId == null) {
+            return;
+        } // end of conditional
+
+        if (detectedId.metadata != null) {
+            telemetry.addLine(String.format("\n==== (ID %d) %s", detectedId.id, detectedId.metadata.name));
+            telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detectedId.ftcPose.x, detectedId.ftcPose.y, detectedId.ftcPose.z));
+            telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detectedId.ftcPose.pitch, detectedId.ftcPose.roll, detectedId.ftcPose.yaw));
+            telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detectedId.ftcPose.range, detectedId.ftcPose.bearing, detectedId.ftcPose.elevation));
+        } else {
+            telemetry.addLine(String.format("\n==== (ID %d) Unknown", detectedId.id));
+            telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detectedId.center.x, detectedId.center.y));
+        } // end of conditional
+    } // end of displayDetectionTelemetry() function
 
     // VisionPortal Stop Function
 
     public void stop() {
         if (visionPortal != null) {
             visionPortal.close();
-        }
-    }
+        } // end of conditional
+    } // end of stop() function
 }
