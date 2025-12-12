f509dbd3e682bcd6199644a95b1ec1dcaf8e95fe
Tumbalcain <tumbalcain@gmail.com>
2025-12-12 09:16:17 +0700

    Update branch (#31)
    * Fix ARCHIVE.md Grammer
    
    * Add ARCHIVE.md and folder (forgot to publish branch uwu)
    
    * Add season image to README.md
    
    * Create Autonomous OpMode
    
    * Create LICENSE
    
    * Update LICENSE url in README.md
    
    * Rename LICENSE to LICENSE.txt
    
    * Update README.md link (Im sorry for minor changes)
    
    * Delete unnecessary shields and sections
    
    * Create starter code MecanumStarterTeleOp.java
    
    * Create autonomous starter for mecanum
    
    * Starter code debug, pipeline duplication and allocation

--- DECODE_season/R2045_DECODE_TeleOp.java
+++ DECODE_season/R2045_DECODE_TeleOp.java
@@ -1,10 +1,9 @@
-package org.firstinspires.ftc.teamcode.R2045;
+package org.firstinspires.ftc.teamcode.R2045.DECODE_season;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.IMU;
-import org.firstinspires.ftc.teamcode.R2045.pipelines.ReusableMecanum;
 
 @TeleOp(name="R2045_DECODE_TeleOp", group="R2045_DECODE")
 public class R2045_DECODE_TeleOp extends OpMode {
